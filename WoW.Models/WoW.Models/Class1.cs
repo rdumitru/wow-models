81f8818b7e442f2134460aa4dbc4a0007c65fe81
Remus Dumitru <remuque@gmail.com>
2015-09-20 21:33:19 +0100

    Initial commit.

---
+++ WoW.Models/WoW.Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoW.Models
+{
+    public class SimpleModel
+    {
+        private string Name { get; set; }
+
+        public SimpleModel(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
